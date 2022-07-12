0b73c7b0fc7984269324d0b29fa9553709f578d1
Toan Phan Bao <baotoan1995@live.com>
2022-07-12 19:31:13 +0700

    Add timeout and interval methods to Observable

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
 import { Observable } from './Observable';
 
 const timeoutObservable$ = Observable.timeout(500);
-const timeoutSubscription = timeoutObservable$.subscribe();
+const timeoutSubscription = timeoutObservable$
+    .subscribe(() => console.log('subscribed'));
 setTimeout(timeoutSubscription.unsubscribe, 1000);
 
 const intervalObservable$ = Observable.interval(500);
-const intervalSubscription = intervalObservable$.subscribe();
+let time = 0;
+const intervalSubscription = intervalObservable$
+    .subscribe(() => {
+        console.log(time);
+        time += 500;
+    });
 setTimeout(intervalSubscription.unsubscribe, 6000);
